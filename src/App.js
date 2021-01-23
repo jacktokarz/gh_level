af639498aac1b0b12980f321536d7158ab738edf
jacktokarz <jacktokarz@gmail.com>
2021-01-23 13:56:36 -0600

    created the app, it is working. yay!

--- src/App.js
+++ src/App.js
@@ -1,22 +1,75 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [firstLevel, setFirstLevel] = useState(0);
+  const [secondLevel, setSecondLevel] = useState(0);
+  const [thirdLevel, setThirdLevel] = useState(0);
+  const [fourthLevel, setFourthLevel] = useState(0);
+
+  const options = [
+    {value: 0, label: 'First Player Level'},
+    {value: 1, label: '1'},
+    {value: 2, label: '2'},
+    {value: 3, label: '3'},
+    {value: 4, label: '4'},
+    {value: 5, label: '5'},
+    {value: 6, label: '6'},
+    {value: 7, label: '7'},
+    {value: 8, label: '8'},
+    {value: 9, label: '9'}
+  ];
+  const active = [firstLevel, secondLevel, thirdLevel, fourthLevel].filter(val => val>0);
+  console.log("active is "+active.length);
+  const scLevel = (active.reduce((a, b) => parseInt(a) + parseInt(b), 0) / active.length) /2;
+
+  const LevelSelect = ({cardinal, level, set}) => {
+    return (
+      <div style={{margin: "12px"}}>
+        <div style={{padding: "4px", fontSize: "80%"}}>{cardinal} Player</div>
+        <select
+          style={{ width: '50px', height: '40px', fontSize: '24px' }}
+          onChange = {(event) => {set(event.target.value)}}
+          options = {options}
+          value = {level}
+        >
+          <option value="0">0</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+        </select>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://cdn.cloudflare.steamstatic.com/steam/apps/780290/extras/20190514-gloomhaven-logoanimated.gif?t=1608655526"
+          style={{margin: "-20% 0 8% 0"}}
+        />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Select The Players' Levels:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{display:"inline-flex"}}>
+          <LevelSelect cardinal="First" level = {firstLevel} set = {setFirstLevel} />
+          <LevelSelect cardinal="Second" level = {secondLevel} set = {setSecondLevel} />
+          <LevelSelect cardinal="Third" level = {thirdLevel} set = {setThirdLevel} />
+          <LevelSelect cardinal="Fourth" level = {fourthLevel} set = {setFourthLevel} />
+        </div>
+        <p>
+          Scenario Level: {Math.ceil(scLevel)}
+        </p>
+        <span style={{fontSize: "16px", color: "grey", marginTop: "-16px"}}>raw: {scLevel}</span>
+
       </header>
     </div>
   );
